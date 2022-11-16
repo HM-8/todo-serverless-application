02c7cc3f7e4dacf048089cd583086aafe1d71c87
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-11-16 12:06:16 +0300

    Add create todo function

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,14 +4,15 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodo } from '../../logic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
-
-    return undefined
+    const userId = getUserId(event)
+    const todo = await createTodo(userId, newTodo)
+    return { statusCode: 201, body: JSON.stringify({ item: todo }) }
+  }
 )
 
 handler.use(
