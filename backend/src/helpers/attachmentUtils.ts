a0fa0145416fbc0f9b1dcc098b1a5ddbb0cf6621
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-11-16 11:18:14 +0300

    Add s3 file logic

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -3,4 +3,24 @@ import * as AWSXRay from 'aws-xray-sdk'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-// TODO: Implement the fileStogare logic
+export class AttachmentUtils {
+    constructor(
+        private readonly s3 = new XAWS.S3({ signatureVersion: 'v4' }),
+        private readonly bucketName = process.env.ATTACHMENTS_S3_BUCKET,
+        private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION) {
+    }
+
+    getAttachmentUrl(todoId: string) {
+        return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
+    }
+
+    getUploadUrl(todoId: string): string {
+        const uploadUrl = this.s3.getSignedUrl('putObject', {
+            Bucket: this.bucketName,
+            Key: todoId,
+            Expires: this.urlExpiration
+        })
+        return uploadUrl as string
+    }
+}
+
